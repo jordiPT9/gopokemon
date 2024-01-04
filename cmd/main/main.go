4cbaea19ca5fcb7bec9f75783feb35acab89252d
jordiPT9 <jordipages9a@gmail.com>
2024-01-04 03:09:08 +0100

    Created top functions for every stat

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/jordipt9/gopokemon/database"
+	"github.com/jordipt9/gopokemon/types"
 )
 
 func main() {
@@ -50,22 +51,64 @@ func main() {
 		"Hitmontop",
 		"Smoochum",
 	}
-	resultList, err := database.TopRawMaxAtk(list)
-	if err != nil {
-		panic(err)
-	}
-	for _, pkmn := range resultList {
-		fmt.Println(pkmn.Name)
-	}
-	fmt.Println("RAW ATK: ", resultList)
+	resultList, err := database.TopRawMaxHp(list)
+	panicErr(err)
+	printPokes(resultList, "RAW HP:")
+	resultList, err = database.TopRawMaxAtk(list)
+	panicErr(err)
+	printPokes(resultList, "RAW ATK:")
+	resultList, err = database.TopRawMaxDef(list)
+	panicErr(err)
+	printPokes(resultList, "RAW DEF:")
+	resultList, err = database.TopRawMaxSpAtk(list)
+	panicErr(err)
+	printPokes(resultList, "RAW SP.ATK:")
+	resultList, err = database.TopRawMaxSpAtk(list)
+	panicErr(err)
+	printPokes(resultList, "RAW SP.ATK:")
+	resultList, err = database.TopRawMaxSpeed(list)
+	panicErr(err)
+	printPokes(resultList, "RAW SPEED:")
 
-	fmt.Println("")
+	resultList, err = database.TopBalancedMaxHp(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED HP:")
 	resultList, err = database.TopBalancedMaxAtk(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED ATK:")
+	resultList, err = database.TopBalancedMaxDef(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED DEF:")
+	resultList, err = database.TopBalancedMaxSpAtk(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED SP.ATK:")
+	resultList, err = database.TopBalancedMaxSpDef(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED SP.DEF:")
+	resultList, err = database.TopBalancedMaxSpeed(list)
+	panicErr(err)
+	printPokes(resultList, "BALANCED SPEED:")
+}
+
+func printPokes(resultList []types.Pokemon, cat string) {
+	fmt.Println(cat)
+	for _, pkmn := range resultList {
+		fmt.Println(
+			pkmn.Name,
+			pkmn.Hp,
+			pkmn.Atk,
+			pkmn.Def,
+			pkmn.SpAtk,
+			pkmn.SpDef,
+			pkmn.Speed,
+			pkmn.Total,
+		)
+	}
+	fmt.Println("")
+}
+
+func panicErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	for _, pkmn := range resultList {
-		fmt.Println(pkmn.Name)
-	}
-	fmt.Println("BALANCED ATK: ", resultList)
 }
--- database/database.go
+++ database/database.go
@@ -49,6 +49,19 @@ func FindByName(name string) (types.Pokemon, error) {
 	return pkmn, nil
 }
 
+func TopRawMaxHp(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+	result := db.Where("LOWER(name) IN ?", names).Order("hp desc").Limit(10).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return pokemons, nil
+}
+
 func TopRawMaxAtk(names []string) ([]types.Pokemon, error) {
 	var pokemons []types.Pokemon
 	for i, name := range names {
@@ -62,6 +75,87 @@ func TopRawMaxAtk(names []string) ([]types.Pokemon, error) {
 	return pokemons, nil
 }
 
+func TopRawMaxDef(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+	result := db.Where("LOWER(name) IN ?", names).Order("def desc").Limit(10).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return pokemons, nil
+}
+
+func TopRawMaxSpAtk(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+	result := db.Where("LOWER(name) IN ?", names).Order("sp_atk desc").Limit(10).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return pokemons, nil
+}
+
+func TopRawMaxSpDef(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+	result := db.Where("LOWER(name) IN ?", names).Order("sp_def desc").Limit(10).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return pokemons, nil
+}
+
+func TopRawMaxSpeed(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+	result := db.Where("LOWER(name) IN ?", names).Order("speed desc").Limit(10).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return pokemons, nil
+}
+
+func TopBalancedMaxHp(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	result := db.Where("LOWER(name) IN ?", names).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	filteredPokemons := make([]types.Pokemon, 0)
+	for _, pokemon := range pokemons {
+		if isMaxHp(pokemon) {
+			filteredPokemons = append(filteredPokemons, pokemon)
+		}
+	}
+
+	sort.Slice(filteredPokemons, func(i, j int) bool {
+		return filteredPokemons[i].Total > filteredPokemons[j].Total
+	})
+
+	if len(filteredPokemons) > 10 {
+		filteredPokemons = filteredPokemons[:10]
+	}
+
+	return filteredPokemons, nil
+}
+
 func TopBalancedMaxAtk(names []string) ([]types.Pokemon, error) {
 	var pokemons []types.Pokemon
 	for i, name := range names {
@@ -73,7 +167,6 @@ func TopBalancedMaxAtk(names []string) ([]types.Pokemon, error) {
 		return nil, result.Error
 	}
 
-	// Filtrar por 'atk' más alto
 	filteredPokemons := make([]types.Pokemon, 0)
 	for _, pokemon := range pokemons {
 		if isMaxAtk(pokemon) {
@@ -81,12 +174,126 @@ func TopBalancedMaxAtk(names []string) ([]types.Pokemon, error) {
 		}
 	}
 
-	// Ordenar por 'total'
 	sort.Slice(filteredPokemons, func(i, j int) bool {
 		return filteredPokemons[i].Total > filteredPokemons[j].Total
 	})
 
-	// Tomar solo los primeros 10
+	if len(filteredPokemons) > 10 {
+		filteredPokemons = filteredPokemons[:10]
+	}
+
+	return filteredPokemons, nil
+}
+
+func TopBalancedMaxDef(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	result := db.Where("LOWER(name) IN ?", names).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	filteredPokemons := make([]types.Pokemon, 0)
+	for _, pokemon := range pokemons {
+		if isMaxDef(pokemon) {
+			filteredPokemons = append(filteredPokemons, pokemon)
+		}
+	}
+
+	sort.Slice(filteredPokemons, func(i, j int) bool {
+		return filteredPokemons[i].Total > filteredPokemons[j].Total
+	})
+
+	if len(filteredPokemons) > 10 {
+		filteredPokemons = filteredPokemons[:10]
+	}
+
+	return filteredPokemons, nil
+}
+
+func TopBalancedMaxSpAtk(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	result := db.Where("LOWER(name) IN ?", names).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	filteredPokemons := make([]types.Pokemon, 0)
+	for _, pokemon := range pokemons {
+		if isMaxSpAtk(pokemon) {
+			filteredPokemons = append(filteredPokemons, pokemon)
+		}
+	}
+
+	sort.Slice(filteredPokemons, func(i, j int) bool {
+		return filteredPokemons[i].Total > filteredPokemons[j].Total
+	})
+
+	if len(filteredPokemons) > 10 {
+		filteredPokemons = filteredPokemons[:10]
+	}
+
+	return filteredPokemons, nil
+}
+
+func TopBalancedMaxSpDef(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	result := db.Where("LOWER(name) IN ?", names).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	filteredPokemons := make([]types.Pokemon, 0)
+	for _, pokemon := range pokemons {
+		if isMaxSpDef(pokemon) {
+			filteredPokemons = append(filteredPokemons, pokemon)
+		}
+	}
+
+	sort.Slice(filteredPokemons, func(i, j int) bool {
+		return filteredPokemons[i].Total > filteredPokemons[j].Total
+	})
+
+	if len(filteredPokemons) > 10 {
+		filteredPokemons = filteredPokemons[:10]
+	}
+
+	return filteredPokemons, nil
+}
+
+func TopBalancedMaxSpeed(names []string) ([]types.Pokemon, error) {
+	var pokemons []types.Pokemon
+	for i, name := range names {
+		names[i] = strings.ToLower(name)
+	}
+
+	result := db.Where("LOWER(name) IN ?", names).Find(&pokemons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	filteredPokemons := make([]types.Pokemon, 0)
+	for _, pokemon := range pokemons {
+		if isMaxSpeed(pokemon) {
+			filteredPokemons = append(filteredPokemons, pokemon)
+		}
+	}
+
+	sort.Slice(filteredPokemons, func(i, j int) bool {
+		return filteredPokemons[i].Total > filteredPokemons[j].Total
+	})
+
 	if len(filteredPokemons) > 10 {
 		filteredPokemons = filteredPokemons[:10]
 	}
@@ -95,9 +302,49 @@ func TopBalancedMaxAtk(names []string) ([]types.Pokemon, error) {
 }
 
 func isMaxAtk(pokemon types.Pokemon) bool {
-	return pokemon.Atk >= pokemon.Hp &&
-		pokemon.Atk >= pokemon.Def &&
-		pokemon.Atk >= pokemon.SpAtk &&
-		pokemon.Atk >= pokemon.SpDef &&
-		pokemon.Atk >= pokemon.Speed
+	return pokemon.Atk > pokemon.Hp &&
+		pokemon.Atk > pokemon.Def &&
+		pokemon.Atk > pokemon.SpAtk &&
+		pokemon.Atk > pokemon.SpDef &&
+		pokemon.Atk > pokemon.Speed
+}
+
+func isMaxHp(pokemon types.Pokemon) bool {
+	return pokemon.Hp > pokemon.Atk &&
+		pokemon.Hp > pokemon.Def &&
+		pokemon.Hp > pokemon.SpAtk &&
+		pokemon.Hp > pokemon.SpDef &&
+		pokemon.Hp > pokemon.Speed
+}
+
+func isMaxDef(pokemon types.Pokemon) bool {
+	return pokemon.Def > pokemon.Hp &&
+		pokemon.Def > pokemon.Atk &&
+		pokemon.Def > pokemon.SpAtk &&
+		pokemon.Def > pokemon.SpDef &&
+		pokemon.Def > pokemon.Speed
+}
+
+func isMaxSpAtk(pokemon types.Pokemon) bool {
+	return pokemon.SpAtk > pokemon.Hp &&
+		pokemon.SpAtk > pokemon.Atk &&
+		pokemon.SpAtk > pokemon.Def &&
+		pokemon.SpAtk > pokemon.SpDef &&
+		pokemon.SpAtk > pokemon.Speed
+}
+
+func isMaxSpDef(pokemon types.Pokemon) bool {
+	return pokemon.SpDef > pokemon.Hp &&
+		pokemon.SpDef > pokemon.Atk &&
+		pokemon.SpDef > pokemon.Def &&
+		pokemon.SpDef > pokemon.SpAtk &&
+		pokemon.SpDef > pokemon.Speed
+}
+
+func isMaxSpeed(pokemon types.Pokemon) bool {
+	return pokemon.Speed > pokemon.Hp &&
+		pokemon.Speed > pokemon.Atk &&
+		pokemon.Speed > pokemon.Def &&
+		pokemon.Speed > pokemon.SpAtk &&
+		pokemon.Speed > pokemon.SpDef
 }
